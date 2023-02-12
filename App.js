aad0e515b14bfda93b45da72c4d17558bb4c1cb4
Kallin Messerer <khmesserer@gmail.com>
2023-02-12 11:00:44 -0600

    First Commit

--- App.js
+++ App.js
@@ -3,11 +3,29 @@ import { useState } from 'react';
 import { View, Text, Image, TextInput, Pressable, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
 
 function HomeScreen({ navigation }) {
 
-  const [servings, setServings] = useState(1);
+  const [isValid, setIsValid] = useState(false);
+  const [servings, setServings] = useState(0);
   const homeImage = require("./assets/images/bruschetta.png");
+
+  function isValidInput(input) {
+    try {
+      if (input != null) {
+        var numServings = parseInt(input);
+        if (numServings > 0){
+          setServings(numServings);
+          return true;
+        }
+      }
+    } catch (error) {}
+    return false;
+  }
 
   return (
     <View style={styles.container}>
@@ -15,11 +33,13 @@ function HomeScreen({ navigation }) {
       <Image style={styles.elementContainer} source={homeImage} />
       <View style={styles.elementContainer}>
         <TextInput style={styles.input} placeholder={"Enter the Number of servings"}
-          onChangeText={(newText) => setServings(newText)}
+          onChangeText={newText => setIsValid(isValidInput(newText))}
         />
       </View>
       <View style={styles.buttonContainer}>
-        <Pressable onPress={() => navigation.navigate('Recipe')}>
+        <Pressable 
+          onPress={() => isValid ? navigation.navigate('Recipe', {numServings: servings}) : alert('Invalid Entry')}
+        >
           <Text style={styles.buttonText}>Veiw Recipe</Text>
         </Pressable>
       </View>
@@ -27,10 +47,25 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function RecipeScreen() {
+function RecipeScreen({ route, navigation }) {
+  const {numServings} = route.params;
+
   return (
     <View style={styles.container}>
-      <Text>Recipe Screen</Text>
+      <Text style={styles.title}>Bruschetta</Text>
+      <View style={styles.recipeContainer}>
+        <Text style={styles.recipe.title}>Ingredients</Text>
+        <Text style={styles.recipe.content}>
+          {4 * numServings} plum tomatoes{'\n'}
+          {6 * numServings} basil leaves{'\n'}
+          {3 * numServings} garlic cloves, chopped{'\n'}
+          {3 * numServings} TB olive oil
+        </Text>
+        <Text style={styles.recipe.title}>Directions</Text>
+        <Text style={styles.recipe.content}>
+          Combine the Ingredients. Add salt to taste. Top French bread slices with mixture.
+        </Text>
+      </View>
     </View>
   );
 }
@@ -40,17 +75,18 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator 
+        initialRouteName='Home'
+        screenOptions={{
+          title: 'Healthy Recipes',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+        }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{
-            title: 'Healthy Recipes',
-            headerStyle: {
-              backgroundColor: '#f4511e'
-            },
-            headerTintColor: '#fff',
-          }}
         />
         <Stack.Screen
           name="Recipe"
@@ -61,13 +97,6 @@ export default function App() {
   );
 }
 
-function isValidInput( navigation, servings) {
-  if (isNaN(servings) && servings < 1) {
-    alert('Invalid Entry');
-  } else {
-    navigation.navigate('Recipe', servings);
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -81,12 +110,26 @@ const styles = StyleSheet.create({
     color: "#000",
   },
   elementContainer: {
-    marginVertical: 10
+    marginVertical: 10,
+  },
+  recipeContainer: {
+    marginHorizontal: 20,
+  },
+  recipe: {
+    color: '#000',
+    title: {
+      marginTop: 20,
+      fontSize: 35,
+    },
+    content: {
+      fontSize: 25,
+      marginLeft: 20,
+    }
   },
   input: {
      height: 40,
      fontSize: 20,
-     textAlign: "center",
+     textAlign: 'center',
   },
   buttonContainer: {
     backgroundColor: "#585858",
